d7a8fcff863ef96ee14116fdad8f3ee53ff860f5
marvinwener <45437696+marvinwener@users.noreply.github.com>
2021-06-10 09:56:29 +0200

    Update hello-world.cc
    Fixt issue

--- cpp-tutorial/stage1/main/hello-world.cc
+++ cpp-tutorial/stage1/main/hello-world.cc
@@ -14,12 +14,12 @@ void print_localtime() {
 
 int main(int argc, char** argv) {
   std::string who = "world";
-  int x;
-  x;
+  int x=0;
+
   if (argc > 1) {
     who = argv[1];
   }
   std::cout << get_greet(who) << std::endl;
   print_localtime();
-  return 0;
+  return x;
 }
